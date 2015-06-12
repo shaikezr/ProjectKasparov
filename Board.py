45bc6b93a8547b87e0972d2f8218f34d56a9d3f4
shanglun <shanglunwang@gmail.com>
2015-06-11 22:16:24 -0700

    Separate out Gameboard
    Reinforceplayer needs to access new boards in order to consider
    hypothetical moves.

---
+++ Board.py
@@ -0,0 +1,53 @@
+import copy
+
+class GameBoard(object):
+	def __init__(self):
+		self.gameboard = [[0,0,0],[0,0,0],[0,0,0]]
+	
+	def __init__(self,board):
+		self.gameboard=copy.deepcopy(board)
+	
+	def is1(self,row,col): #check if p1 has won
+		if self.gameboard[row][col] == 1:
+			return True
+		else:
+			return False
+		
+	def is2(self, row, col): #check if p2 has won
+		if self.gameboard[row][col] == 2:
+			return True
+		else:
+			return False
+	
+	def printrow(self, row):
+		c1 = ' '
+		c2 = ' '
+		c3 = ' '
+		if self.is1(row,0):
+			c1 = 'o'
+		elif self.is2(row,0):
+			c1 = 'x'
+			
+		if self.is1(row,1):
+			c2 = 'o'
+		elif self.is2(row,1):
+			c2 = 'x'
+			
+		if self.is1(row,2):
+			c3 = 'o'
+		elif self.is2(row,2):
+			c3 = 'x'
+		row = "+ {0} | {1} | {2} +".format(c1,c2,c3);
+		print(row)
+	
+	def printboard(self):
+		print("+++++++++++++")
+		self.printrow(0)
+		print("+-----------+")
+		self.printrow(1)
+		print("+-----------+")
+		self.printrow(2)
+		print("+++++++++++++")
+	
+	def makemove(self,turn,row,col):
+		self.gameboard[row][col] = turn
--- Kasparov.py
+++ Kasparov.py
@@ -6,59 +6,8 @@
 # Second mover will be 2
 
 import Players
-
-class GameBoard(object):
-	def __init__(self):
-		self.gameboard = [[0,0,0],[0,0,0],[0,0,0]]
-	
-	def is1(self,row,col): #check if p1 has won
-		if self.gameboard[row][col] == 1:
-			return True
-		else:
-			return False
+import GameBoard from Board
 		
-	def is2(self, row, col): #check if p2 has won
-		if self.gameboard[row][col] == 2:
-			return True
-		else:
-			return False
-	
-	
-	
-	def printrow(self, row):
-		c1 = ' '
-		c2 = ' '
-		c3 = ' '
-		if self.is1(row,0):
-			c1 = 'o'
-		elif self.is2(row,0):
-			c1 = 'x'
-			
-		if self.is1(row,1):
-			c2 = 'o'
-		elif self.is2(row,1):
-			c2 = 'x'
-			
-		if self.is1(row,2):
-			c3 = 'o'
-		elif self.is2(row,2):
-			c3 = 'x'
-		row = "+ {0} | {1} | {2} +".format(c1,c2,c3);
-		print(row)
-	
-	def printboard(self):
-		print("+++++++++++++")
-		self.printrow(0)
-		print("+-----------+")
-		self.printrow(1)
-		print("+-----------+")
-		self.printrow(2)
-		print("+++++++++++++")
-	
-	def makemove(self,turn,row,col):
-		self.gameboard[row][col] = turn
-		
-
 class Game(object):
 	def __init__(self, Player1, Player2):
 		self.player1 = Player1
