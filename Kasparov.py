682b8aa340e2f588e13bf78b6386de3e3a85719f
shanglun <shanglunwang@gmail.com>
2015-06-17 19:13:18 -0700

    Checkwin static
    Checkwin is now a static method for use by the genetic algorithm.

--- Kasparov.py
+++ Kasparov.py
@@ -16,40 +16,49 @@ class Game(object):
 		self.board = GameBoard()
 		self.turn = 1
 		
-	def checkwin(self): #check if someone has won
+	@staticmethod	
+	def checkwin(board): #check if someone has won
+		curboard  = 0 
+		if isinstance(board,GameBoard): #typechecking is not working consistently. 
+			curboard = board
+		elif isinstance(board, list):
+			curboard = GameBoard(board)
+		else:
+			return 1 #SOMETHING WENT WRONG.
+			
 		if(
 			#horizontals
-			(self.board.is1(0,0) and self.board.is1(0,1) and self.board.is1(0,2)) or
-			(self.board.is1(1,0) and self.board.is1(1,1) and self.board.is1(1,2)) or
-			(self.board.is1(2,0) and self.board.is1(2,1) and self.board.is1(2,2)) or
+			(curboard.is1(0,0) and curboard.is1(0,1) and curboard.is1(0,2)) or
+			(curboard.is1(1,0) and board.is1(1,1) and curboard.is1(1,2)) or
+			(curboard.is1(2,0) and curboard.is1(2,1) and curboard.is1(2,2)) or
 			#verticals
-			(self.board.is1(0,0) and self.board.is1(1,0) and self.board.is1(2,0)) or
-			(self.board.is1(0,1) and self.board.is1(1,1) and self.board.is1(2,1)) or
-			(self.board.is1(0,2) and self.board.is1(1,2) and self.board.is1(2,2)) or
+			(curboard.is1(0,0) and curboard.is1(1,0) and curboard.is1(2,0)) or
+			(curboard.is1(0,1) and curboard.is1(1,1) and curboard.is1(2,1)) or
+			(curboard.is1(0,2) and curboard.is1(1,2) and curboard.is1(2,2)) or
 			#diagonals
-			(self.board.is1(0,0) and self.board.is1(1,1) and self.board.is1(2,2)) or
-			(self.board.is1(0,2) and self.board.is1(1,1) and self.board.is1(2,0))
+			(curboard.is1(0,0) and curboard.is1(1,1) and curboard.is1(2,2)) or
+			(curboard.is1(0,2) and curboard.is1(1,1) and curboard.is1(2,0))
 		  ):
 			return 1 #1 wins
 		elif (
 			#horizontals
-			(self.board.is2(0,0) and self.board.is2(0,1) and self.board.is2(0,2)) or
-			(self.board.is2(1,0) and self.board.is2(1,1) and self.board.is2(1,2)) or
-			(self.board.is2(2,0) and self.board.is2(2,1) and self.board.is2(2,2)) or
+			(curboard.is2(0,0) and curboard.is2(0,1) and curboard.is2(0,2)) or
+			(curboard.is2(1,0) and curboard.is2(1,1) and curboard.is2(1,2)) or
+			(curboard.is2(2,0) and curboard.is2(2,1) and curboard.is2(2,2)) or
 			#verticals
-			(self.board.is2(0,0) and self.board.is2(1,0) and self.board.is2(2,0)) or
-			(self.board.is2(0,1) and self.board.is2(1,1) and self.board.is2(2,1)) or
-			(self.board.is2(0,2) and self.board.is2(1,2) and self.board.is2(2,2)) or
+			(curboard.is2(0,0) and curboard.is2(1,0) and curboard.is2(2,0)) or
+			(curboard.is2(0,1) and curboard.is2(1,1) and curboard.is2(2,1)) or
+			(curboard.is2(0,2) and curboard.is2(1,2) and curboard.is2(2,2)) or
 			#diagonals
-			(self.board.is2(0,0) and self.board.is2(1,1) and self.board.is2(2,2)) or
-			(self.board.is2(0,2) and self.board.is2(1,1) and self.board.is2(2,0))
+			(curboard.is2(0,0) and curboard.is2(1,1) and curboard.is2(2,2)) or
+			(curboard.is2(0,2) and curboard.is2(1,1) and curboard.is2(2,0))
 		  ):
 			return 2 # 2 wins
 		else:
 			has_valid_move = False
 			for i in range(3):
 				for j in range(3):
-					if not self.board.is1(i,j) and not self.board.is2(i,j):
+					if not curboard.is1(i,j) and not curboard.is2(i,j):
 						has_valid_move = True
 						break;
 				if has_valid_move:
@@ -74,18 +83,18 @@ class Game(object):
 			self.turn = 1
 		
 	def play(self):
-		while self.checkwin() == 0:
+		while self.checkwin(self.board) == 0:
 			self.board.printboard()
 			self.prompt_for_move()
 			
 		self.board.printboard();
-		if self.checkwin() == 1:
+		if self.checkwin(self.board) == 1:
 			self.player1.report_win()
 			self.player2.report_loss()
-		elif self.checkwin() == 2:
+		elif self.checkwin(self.board) == 2:
 			self.player2.report_win()
 			self.player1.report_loss()
-		elif self.checkwin() == -1:
+		elif self.checkwin(self.board) == -1:
 			self.player1.report_draw()
 			self.player2.report_draw()
 		else:
@@ -93,7 +102,7 @@ class Game(object):
 
 			
 ReinforceInt = Reinforce.ReinforceInterface()
-Player1 = ReinforceInt.generate_reinforceplayer(1)
+Player1 = Players.RandomPlayer(1)#ReinforceInt.generate_reinforceplayer(1)
 Player2 = Players.RandomPlayer(2)
-KaspGame = Game(Player1, Player2);
+KaspGame = Game(Player1, Player2)
 KaspGame.play();
